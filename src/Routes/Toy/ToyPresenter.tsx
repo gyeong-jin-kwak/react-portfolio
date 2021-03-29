ec17929d2bdabe464d8fbdd7a28f44cbb9b9d40b
곽경진 <kgj1212_@naver.com>
2021-03-29 20:39:56 +0900

    [feat] toyprocjet add card

--- src/Routes/Toy/ToyPresenter.tsx
+++ src/Routes/Toy/ToyPresenter.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ProjectCard from "Components/Card/index";
 
 const ToyPresenter: React.VFC = () => {
-	return <span>Toy</span>;
+	return (
+		<div className="container container--card">
+			<div className="content-wrap">
+				<ProjectCard />
+			</div>
+		</div>
+	);
 };
 
 export default ToyPresenter;
