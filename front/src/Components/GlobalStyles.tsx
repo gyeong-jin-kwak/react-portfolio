9ad046e14e1aa02f25204a59b4f41248ddf2b20e
곽경진 <kgj1212_@naver.com>
2021-03-31 23:46:23 +0900

    [fix] add content-wrap top bottom padding

--- front/src/Components/GlobalStyles.tsx
+++ front/src/Components/GlobalStyles.tsx
@@ -30,6 +30,7 @@ export default createGlobalStyle`
         display: flex;
         width: 100%;
         margin: 0 auto;
+        padding: 2rem 0;
         flex-wrap: wrap;
         @media only screen and (min-width: ${BREAK_POINT_PC}px) {
           max-width: 1056px;
